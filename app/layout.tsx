39e638af19bedb32b53c3e5da0519718392aaff4
Josh Atencio <josha10ci045@gmail.com>
2025-12-15 11:03:33 -0600

    Add explicit bg-white to layout wrapper and main element

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,9 +80,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} ${raleway.variable} font-sans antialiased bg-white relative min-h-screen flex flex-col`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange forcedTheme="light">
-          <div className="flex flex-col min-h-screen w-full max-w-none mx-auto">
+          <div className="flex flex-col min-h-screen w-full max-w-none mx-auto bg-white">
             <Header />
-            <main className="flex-1 relative z-10 w-full">{children}</main>
+            <main className="flex-1 relative z-10 w-full bg-white">{children}</main>
             <Footer />
           </div>
 
